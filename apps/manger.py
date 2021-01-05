ce20947c342cd697d3d2761a928ea794af823b43
dorians5689 <dorians5689@gmail.com>
2021-01-05 21:37:17 +0800

    重构代码

---
+++ apps/manger.py
@@ -0,0 +1,14 @@
+# !/bin/env python
+# -*- coding=utf-8 -*-
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+from apps import little_config
+from apps.logic import create_app
+
+port = int(little_config().get("project_port", 5010))
+app = create_app()
+
+if __name__ == '__main__':
+    # app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='127.0.0.1', port=port)
